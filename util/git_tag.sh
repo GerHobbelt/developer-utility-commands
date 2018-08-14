e1249de7b0b882ad8e89aaa11d898cc5e385cde0
Ger Hobbelt <ger@hobbelt.com>
2018-08-14 17:24:35 +0200

    git_tag: add support for `lerna`-based multipackage repositories, e.g. `babel`

--- util/git_tag.sh
+++ util/git_tag.sh
@@ -14,7 +14,17 @@ echo "git repository base directory: $wd"
 cd "$wd"
 
 if [ -f package.json ]; then
-  node -e 'var pkg = require("./package.json"); console.log(pkg.version);' | xargs git tag
+  VERSION=$(node -e 'var pkg = require("./package.json"); console.log(pkg.version ? pkg.version : "");')
+fi
+if [ -f lerna.json ]; then
+  VERSION=$(node -e 'var pkg = require("./lerna.json"); console.log(pkg.version ? pkg.version : "");')
+fi
+echo "Version: $VERSION"
+
+if [ -n "$VERSION" ]; then
+	git tag "$VERSION"
+elif [ ! -n "$VERSION" ]; then
+  echo "This repo doesn't come with a VERSION number in its package.json or lerna.json file"
 else
   echo "This repo doesn't come with a package.json file"
 fi
