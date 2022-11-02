e482213b17ef8f66ea850409aa3a692d8b8e957c
Ger Hobbelt <ger@hobbelt.com>
2022-11-02 11:37:57 +0100

    `fix-github-origin.sh`: fixup all submodule URLs to use HTTPS: at all times. Meanwhile we will get **our own repositories registered as SSH URLs** so we can employ the most brutal **no-interactivity** git configuration available: whenever necessary, git will fetch our suitable SSH key for `git push` et al (as invoked via `git_pull_push.sh` & others) while we *still will not be bothered by obnoxious remotes (gitlab, ...) registered with our development tree.
    This DOES NOT require the use of a github PAT (Private Access Token) as we'll be using GIT/SSH keys instead, which I've found are much less cause for obnoxious login/access popup dialogs which only serve to irk me as they lock up batch mode git access (`git_pull_push.sh`, for example, is often run in the background while I work on other stuff, thus acting like a batch job. Any lockup/delay thanks to any unwanted "please log in" queries is highly undesirable - the script should simply fail those particular commands and continue with its job.
    
    This is counter to current github advisory, so you know what you are doing.
    
    - https://docs.github.com/en/get-started/getting-started-with-git/about-remote-repositories#cloning-with-https-urls
    - https://stackoverflow.com/questions/46645843/where-to-store-my-git-personal-access-token
    - https://docs.github.com/en/get-started/getting-started-with-git/caching-your-github-credentials-in-git
    - https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token

--- fix-github-origin.sh
+++ fix-github-origin.sh
@@ -3,8 +3,14 @@
 
 for f in $( find -type f -name '.gitmodules' ) ; do 
 	echo $f 
-	sed -i -e 's/https:\/\/github.com\/GerHobbelt/git@github.com:GerHobbelt/g' $f 
 	pushd $( dirname $f ) 
+	echo "--- Setting up all submodules to use HTTPS:"
+	sed -i -E -e 's/git@github.com:/https:\/\/github.com\//g' .gitmodules
+	# now make it all SSH access for me, so we can run with git interactivity DISABLED throughout, while git will automatically use our SSH key where applicable (git push, et al)
+	echo "--- Tweaking my own forks to use SSH instead, then REGISTER those URLs for each submodule to sync:"
+	sed -i -E -e 's/https:\/\/github.com\/GerHobbelt/git@github.com:GerHobbelt/g' .gitmodules
 	git submodule sync 
+	echo "--- And hiding that little patch for the whole wide world... :-)"
+	sed -i -E -e 's/git@github.com:/https:\/\/github.com\//g' .gitmodules
 	popd 
 done
