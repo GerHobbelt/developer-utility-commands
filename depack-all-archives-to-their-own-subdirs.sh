786f34a45406398536e274e524929aca6d4215f1
Ger Hobbelt <ger@hobbelt.com>
2026-08-02 13:33:13 +0200

    - improved depack-all-archives-to-their-own-subdirs.sh: support multi-volume RAR archives as well as regular single-file ones. - also be more careful about depacking any lingering TAR file (which may have been produced by extracting a `.tar.bz2` or similar archive): only do the tar work for *actual* TAR files: `for f in *.tar ; do...` has the regrettable habit to produce one round of `f=*.tar` when there's not a single tar file to be found, resulting in depacker errors being reported and potentially confusing the user.

--- depack-all-archives-to-their-own-subdirs.sh
+++ depack-all-archives-to-their-own-subdirs.sh
@@ -3,6 +3,11 @@
 #
 #
 
+#set -x
+#set -v
+#set -o verbose
+#set -o xtrace 
+
 if [ -z "$*" ] ; then
 	find -type f -iregex '.*[.]\(rar\|zip\|7z\|tar[.]gz\|tar[.]bz2\|tar\)' -exec "$0" "{}" \;
 else
@@ -11,20 +16,40 @@ else
 	echo "DEPACK $1" 
 	pushd "$( dirname "$1" )"                                         > /dev/null
 	ZIPFILE="$( basename "$1" )"
-	ZIPDIR="$( echo "$ZIPFILE" | sed -E -e 's/[.]zip//i' -e 's/[.]rar//i' -e 's/[.]7z//i' -e 's/[.]tar[.a-z]*//i' )"
+	ZIPDIR="$( echo "$ZIPFILE" | sed -E -e 's/[.]zip//i' -e 's/[.]part[0-9][0-9]*[.]rar//i' -e 's/[.]rar//i' -e 's/[.]7z//i' -e 's/[.]tar[.a-z]*//i' )"
 	echo "ZIPDIR = $ZIPDIR"
 	echo "ZIPFILE = $ZIPFILE"
 	mkdir "$ZIPDIR"
 	if [ -d "$ZIPDIR" ] ; then
 		cd "$ZIPDIR"
-		/c/Program\ Files/7-Zip-Zstandard/7z.exe x -bt  "../$ZIPFILE"
-		RETURN_CODE=$?
-		echo "EXIT CODE: $RETURN_CODE"
-		if [ "$RETURN_CODE" = 0 ] ; then
-		    echo "DELETE DEPACKED ARCHIVE FILE: $1"
-			rm "../$ZIPFILE"
-		fi
+
+		pwd
+
+		case "$ZIPFILE" in
+        *.part1.rar|*.part01.rar|*.part0001.rar) 
+		  echo "Process multipart RAR..."
+		  /c/Program\ Files/7-Zip-Zstandard/7z.exe x -bt  "../$ZIPFILE"
+		  RETURN_CODE=$?
+		  echo "EXIT CODE: $RETURN_CODE"
+		  if [ "$RETURN_CODE" = 0 ] ; then
+			  echo "DELETE DEPACKED MULIPART ARCHIVE FILE: $1"
+			  rm "../$ZIPDIR.part*.rar"
+		  fi
+		  ;;
+
+		*)
+		  /c/Program\ Files/7-Zip-Zstandard/7z.exe x -bt  "../$ZIPFILE"
+		  RETURN_CODE=$?
+		  echo "EXIT CODE: $RETURN_CODE"
+		  if [ "$RETURN_CODE" = 0 ] ; then
+			  echo "DELETE DEPACKED ARCHIVE FILE: $1"
+			  rm "../$ZIPFILE"
+		  fi
+		  ;;
+		esac
+
 		for tar in *.tar ; do
+		  if test -f "./$tar" ; then
 			/c/Program\ Files/7-Zip-Zstandard/7z.exe x -bt  "./$tar"
 			RETURN_CODE=$?
 			echo "EXIT CODE: $RETURN_CODE"
@@ -32,6 +57,7 @@ else
 				echo "DELETE DEPACKED TAR FILE: $tar"
 				rm "./$tar"
 			fi
+		  fi
 		done
 	fi
 	popd                                                              > /dev/null
